a05afb3e31c9deaab5ded077a7fb03dfa0dd436c
Owen Treanor <owentreanor03@gmail.com>
2025-11-30 13:43:08 -0500

    figured out how to authenticate firebase on the express server

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,12 +1,15 @@
 import TableEditor from "../lecture_components/TableEditor";
+import PingServer from "./PingServer";
 
 function Landing() {
-	return (
-		<>
-			<p>Landing</p>
-			<TableEditor></TableEditor>
-		</>
-	);
+  return (
+    <>
+      <p>Landing</p>
+      <PingServer />
+	  <br/>
+      <TableEditor></TableEditor>
+    </>
+  );
 }
 
 export default Landing;
