8cf3e6efa5cc7a0b62a01bb46aa997a99a02e2a4
Jimmy Colgan <jcolgan1@stevens.edu>
2025-12-09 15:28:07 -0500

    filter collaborated wikis from owner section

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -120,9 +120,14 @@ function Home() {
 
 							<h4 id="owner">OWNER</h4>
 							<p className="small text-muted">Public and private wikis you have ownership of.</p>
-							{wikisData && wikisData.map((wiki) => 
-								<WikiCard wiki={wiki}/>
-							)}
+							{wikisData &&
+								wikisData
+									.filter(wiki => wiki.owner === currentUser.uid)
+									.map(wiki => (
+									<WikiCard key={wiki._id} wiki={wiki} />
+									))
+}
+
 							<h4 id="collaborator">COLLABORATOR </h4>
 							<p className="small text-muted">Public and private wikis where you aren't the owner but have been granted exclusive editing permissions.</p>
 							<p className="small text-muted">Does not include public wikis where editing is available to all users.</p>
