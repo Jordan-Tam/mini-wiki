f95f754790e4aff914cbe236dffb0e11de0d6b9d
KCGD <kcgdhosting@gmail.com>
2025-12-12 22:10:15 -0500

    sync with old chat frontend

--- backend/routes/socket/chat_router.ts
+++ backend/routes/socket/chat_router.ts
@@ -1,19 +1,35 @@
-import { SocketRouter } from "../../lib/ws/socket_server";
-import { Broadcaster } from "../../lib/ws/broadcast";
 import { MessageEvent } from "ws";
+import user_data_functions from "../../data/users";
+import { Broadcaster } from "../../lib/ws/broadcast";
+import { SocketRouter } from "../../lib/ws/socket_server";
 
 const chats:{[key:string]: Broadcaster} = {};
 
 export const ChatSocket:SocketRouter = async(socket, req, params): Promise<void> => {
-    let usr = params[":usr"];
     let id = params[":id"];
+
+    /**
+     * Check user is logged in / exists
+     */
+    let fb_user_id = (req as any).user?.uid;
+    if(!fb_user_id) {
+        return socket.close(1008, `You must be logged in to connect to chat.`);
+    }
+    
+    let user = await user_data_functions.getUserByFirebaseUID(fb_user_id);
+    if(!user) {
+        return socket.close(1008, `User not found.`);
+    }
+
+    let user_id = user._id;
+    let username = user.username;
 
     /**
      * Create socket handlers
      */
     const _onclose = async() => {
         try {
-            await chats[id].remove(usr);
+            await chats[id].remove(user_id);
         } catch (e) {
             // user removed elsewhere?
         }
@@ -29,7 +45,7 @@ export const ChatSocket:SocketRouter = async(socket, req, params): Promise<void>
     // right now it's just direct forwarding
     // will need to add XSS and other protections
     const _onmessage = async(message:MessageEvent) => {
-        await chats[id].broadcast(`<${usr}>: ${String(message.data)}`);
+        await chats[id].broadcast(`<${username}>: ${String(message.data)}`);
     }
 
     const _dropEventListeners = async() => {
@@ -54,8 +70,8 @@ export const ChatSocket:SocketRouter = async(socket, req, params): Promise<void>
     }
 
     // add user to broadcaster
-    await chats[id].add(usr, socket, true);
+    await chats[id].add(user_id, socket, true);
 
     // send user join
-    await chats[id].broadcast(`<${usr}> has joined the chat!`);
+    await chats[id].broadcast(`<${username}> has joined the chat!`);
 }
