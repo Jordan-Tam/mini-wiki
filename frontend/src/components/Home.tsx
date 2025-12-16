654062cd8ea7807d8e97bb9ee52cec0ba0f6de62
KCGD <kcgdhosting@gmail.com>
2025-12-16 17:29:52 -0500

    imports stuff

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,7 +2,7 @@ import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext.jsx";
 import WikiCard from "./cards/WikiCard.tsx";
-import CreateWikiModal from "./modals/CreateWikiModal.js";
+import CreateWikiModal from "./modals/CreateWikiModal.tsx";
 
 function Home() {
 
